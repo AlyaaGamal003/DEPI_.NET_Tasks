f3b383ca4d0831b69b4b47e7e0c2e49fcea63589
AlyaaGamal003 <galyaa742004@gmail.com>
2025-07-30 16:20:23 +0300

    C#FirstTaskCommit

---
+++ C#_TASKS/Task_01_calculator.cs
@@ -0,0 +1,43 @@
+﻿namespace Task1_DEPI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Calculater task
+            Console.WriteLine("Hello!\r\nInput the first number:\n");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nInput the second number:\n");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nWhat do you want to do with those numbers?\r\n[A]dd\r\n[S]ubtract\r\n[M]ultiply\r\n");
+            char ch = char.Parse(Console.ReadLine());
+            switch (ch)
+            {
+                case 'A':
+                case 'a':
+                    Console.WriteLine("\n" + num1 + " + " + num2 + " = " + (num1 + num2));
+                    break;
+
+                case 'S':
+                case 's':
+                    Console.WriteLine("\n" + num1 + " - " + num2 + " = " + (num1 - num2));
+                    break;
+
+                case 'M':
+                case 'm':
+                    Console.WriteLine("\n" + num1 + " * " + num2 + " = " + (num1 * num2));
+                    break;
+
+                default:
+                    Console.WriteLine("\nInvalid option");
+                    break;
+            }
+            Console.WriteLine("\nPress any key to close\r\n");
+            Console.ReadKey();
+
+
+            #endregion
+
+        }
+    }
+}
