52b4a0ebf03070393ed924e193ad503facf16521
AlyaaGamal003 <galyaa742004@gmail.com>
2025-09-15 21:17:14 +0300

    JS & Bootstrap

---
+++ JavaScrit_TASK/script.js
@@ -0,0 +1,26 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  const adviceIdEl = document.querySelector(".advice-id");
+  const adviceTextEl = document.querySelector(".advice-text");
+  const generateBtn = document.getElementById("generateBtn");
+
+
+  async function fetchAdvice() {
+    try {
+      const response = await fetch("https://api.adviceslip.com/advice", { cache: "no-cache" });
+      const data = await response.json();
+      const slip = data.slip;
+
+      adviceIdEl.textContent = `Advice #${slip.id}`;
+      adviceTextEl.textContent = `"${slip.advice}"`;
+    } catch (error) {
+      adviceIdEl.textContent = "Error";
+      adviceTextEl.textContent = "حدث خطأ في تحميل النصيحة 😢";
+      console.error("Error fetching advice:", error);
+    }
+  }
+
+ 
+  fetchAdvice();
+  generateBtn.addEventListener("click", fetchAdvice);
+});
